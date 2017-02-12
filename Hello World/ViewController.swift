bd9db365103c58040ade3f417b90a3db553ec778
CunliffeAdrian <adriancunliffe@Adrians-MacBook-Pro.local>
2017-02-12 20:04:09 +0000

    First Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Adrian Cunliffe on 05/02/2017.
+//  Copyright © 2017 Adrian Cunliffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Background: UIImageView!
+    
+    @IBOutlet weak var Titleimage: UIImageView!
+    
+    @IBOutlet weak var WelcomeBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func WelcomeBtnPressed(_ sender: Any) {
+        Titleimage.isHidden=false
+        Background.isHidden=false
+        
+    }
+    
+    @IBAction func WelomeBtnReleased(_ sender: Any) {
+        Titleimage.isHidden=true
+        Background.isHidden=true
+        }
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello-World
+first app
+
+I've been taking the Udemy Course by Mark Price and this is the first app that we created.
+I'm just testing how Git hub works etc. Looking to maybe delete this in the future.
